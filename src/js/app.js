a37595682fcccfd8b945cd17c4e6dae003f0a29d
tola <tolavolaoke@hotmail.co.uk>
2019-10-13 21:56:10 +0100

    created variables in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,6 @@
 console.log("yeah it works!");
 
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const result_p = document.querySelector(".result > p");
