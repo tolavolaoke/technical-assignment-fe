a61889759e7e51f37c21ac7c54e35ccdd8033b38
Omotola Olaoke <oolaoke@monsoon.co.uk>
2019-10-22 08:58:23 +0100

    removed the main.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 console.log("yeah it works!");
 
+const userScore = 0;
+const compScore = 0;
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
