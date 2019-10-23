3633b8e0564709941cdb1698f5c084feef47aba1
Omotola Olaoke <oolaoke@monsoon.co.uk>
2019-10-23 09:05:39 +0100

    added scores to the html and added variables to the js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,13 @@ console.log("yeah it works!");
 
 const userScore = 0;
 const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compterScore_span = document.getElementById("computer-score");
+const scores_div = document.querySelector (".scores");
+
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 const result_p = document.querySelector(".result > p");
+
+
